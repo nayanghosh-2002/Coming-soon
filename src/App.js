a643ed2469541e37d94e60c047cee697e48a1efb
nayanghosh-2002 <138111956+nayanghosh-2002@users.noreply.github.com>
2025-07-09 19:30:51 +0530

    updated commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
 
 function App() {
+  const [progress, setProgress] = useState(0);
+
+  useEffect(() => {
+    const target = 70;
+    const interval = setInterval(() => {
+      setProgress(prev => {
+        if (prev >= target) {
+          clearInterval(interval);
+          return target;
+        }
+        return prev + 1;
+      });
+    }, 400);
+
+    return () => clearInterval(interval);
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1 className="title">
+        COMING SOON
+        <span className="blinking-dot">
+          <span className="dot bright">.</span>
+          
+        </span><br></br><br></br>
+      </h1>
+
+      <div className="glow-line"></div><br></br><br></br>
+
+      <p className="subtitle">Website Under Construction</p><br></br>
+
+      <div className="progress-container">
+        <div className="progress-bar" style={{ width: `${progress}%` }}></div>
+      </div><br></br><br></br>
+
+      <p className="progress-text">{progress}% done...</p>
     </div>
   );
 }
